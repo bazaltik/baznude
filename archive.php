2ce7b39ee19b0ff80a2e213e20b976cb19374c19
Thomas Boucharel <Thomas@MacBook-Pro-de-Thomas.local>
2014-01-14 15:51:32 +0100

    delete file

--- archive.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-<div class="main">
-	<?php query_posts('posts_per_page=20'); ?>
-	<?php get_template_part('loop'); ?>
-</div>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
